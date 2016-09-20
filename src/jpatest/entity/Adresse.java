8082674e98619d16c9732806c8bfcb4a12b0e8ac
sombrero81 <tead987@gmail.com>
2016-09-20 13:56:43 +0200

    20/sept/2016 14h

---
+++ src/jpatest/entity/Adresse.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jpatest.entity;
+
+import java.io.Serializable;
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author admin
+ */
+@Embeddable
+public class Adresse  {
+
+    private String rue;
+    private Integer numero;
+    private Integer codePstal;
+    private String ville;
+    private String pays;
+    
+}
---
+++ test/jpatest/test/CreateMyDBTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jpatest.test;
+
+import javax.persistence.Persistence;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author admin
+ */
+public class CreateMyDBTest {
+    
+   @Test
+   public void test(){
+       
+       Persistence.createEntityManagerFactory("PU");
+   }
+    
+}
